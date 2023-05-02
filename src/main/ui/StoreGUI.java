5b585be7c445d617be2911fc5950d0191af1bea7
jinkim0517 <jkim71@students.cs.ubc.ca>
2023-05-01 18:00:35 -0700

    move commit

--- src/main/ui/StoreGUI.java
+++ src/main/ui/StoreGUI.java
@@ -3,6 +3,8 @@ package ui;
 import model.*;
 import ui.menus.MainScreen;
 
+import java.util.HashMap;
+
 public class StoreGUI {
     public static void main(String[] args) {
         MainScreen mainScreen = new MainScreen(new Inventory());
